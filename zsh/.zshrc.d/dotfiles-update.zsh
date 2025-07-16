f0bd440bbf0588fc38b301d6b1df7fc2aaf699fb
David Finster <david@fortc.com>
2025-07-16 19:48:02 -0400

    feat: Add auto-update functionality to dotfiles

---
+++ zsh/.zshrc.d/dotfiles-update.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+#
+# dotfiles-update.zsh - Auto-check for dotfiles updates on shell startup
+#
+
+# Run update check on interactive shell startup
+if [[ -o interactive ]]; then
+    dotfiles-update
+fi
