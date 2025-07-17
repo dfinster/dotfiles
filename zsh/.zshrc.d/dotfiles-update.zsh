21052b13057a95cad6d2b2ac25f189399b8a8b44
David Finster <david@fortc.com>
2025-07-16 20:06:00 -0400

    fix: Defer execution

--- zsh/.zshrc.d/dotfiles-update.zsh
+++ zsh/.zshrc.d/dotfiles-update.zsh
@@ -3,7 +3,17 @@
 # dotfiles-update.zsh - Auto-check for dotfiles updates on shell startup
 #
 
-# Run update check on interactive shell startup
+# Run update check on interactive shell startup (deferred)
 if [[ -o interactive ]]; then
-    dotfiles-update
+    # Defer execution to allow shell to fully initialize
+    zle -N check-dotfiles-update
+    check-dotfiles-update() { dotfiles-update }
+    
+    # Schedule to run after shell is ready
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd check-dotfiles-update-once
+    check-dotfiles-update-once() {
+        add-zsh-hook -d precmd check-dotfiles-update-once
+        dotfiles-update
+    }
 fi
