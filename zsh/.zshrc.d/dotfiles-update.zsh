2f71b971087e4f48f64d84d7323e72e981111abd
David Finster <david@fortc.com>
2025-07-16 20:50:44 -0400

    feat: Improve startup time and update notification

--- zsh/.zshrc.d/dotfiles-update.zsh
+++ zsh/.zshrc.d/dotfiles-update.zsh
@@ -5,5 +5,12 @@
 
 # Run update check on interactive shell startup
 if [[ -o interactive ]]; then
-    dotfiles-check-update
+    # Check if background mode is enabled (set DOTFILES_CHECK_BACKGROUND=1 to enable)
+    if [[ "$DOTFILES_CHECK_BACKGROUND" == "1" ]]; then
+        # Run check in background to avoid blocking terminal startup
+        dotfiles-check-update &!
+    else
+        # Run synchronously (default behavior)
+        dotfiles-check-update
+    fi
 fi
