c194ee604b45f6f864c533b8d0f2665aa5063a06
David Finster <david@fortc.com>
2025-07-16 20:31:25 -0400

    fix: Make update a two part process

--- zsh/.zshrc.d/dotfiles-update.zsh
+++ zsh/.zshrc.d/dotfiles-update.zsh
@@ -3,17 +3,7 @@
 # dotfiles-update.zsh - Auto-check for dotfiles updates on shell startup
 #
 
-# Run update check on interactive shell startup (deferred)
+# Run update check on interactive shell startup
 if [[ -o interactive ]]; then
-    # Defer execution to allow shell to fully initialize
-    zle -N check-dotfiles-update
-    check-dotfiles-update() { dotfiles-update }
-    
-    # Schedule to run after shell is ready
-    autoload -Uz add-zsh-hook
-    add-zsh-hook precmd check-dotfiles-update-once
-    check-dotfiles-update-once() {
-        add-zsh-hook -d precmd check-dotfiles-update-once
-        dotfiles-update
-    }
+    dotfiles-check-update
 fi
